9d6900b784c897271b88fe945242037573023162
ahmedosama7450 <a.osama7450@gmail.com>
2022-07-27 15:03:18 +0200

    Setup library structure

---
+++ .storybook/preview.js
@@ -0,0 +1,32 @@
+import { RouterContext } from "next/dist/shared/lib/router-context";
+import * as NextImage from "next/image";
+
+import { OverwindProvider } from "../src/OverwindProvider";
+
+const OriginalNextImage = NextImage.default;
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+export const decorators = [
+  (Story) => (
+    <OverwindProvider>
+      <Story />
+    </OverwindProvider>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  layout: "centered",
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  presets: [require("overwind-ui/tailwind")],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+};
